e7f0f4b7954c2fc704e497918a11a345c89acbbd
rvighnesh <rvighnesh@google.com>
2025-01-12 19:52:32 +0530

    chore: configure basic docs site

--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 > This is not an official set of libraries offered by Google. I built these on
 > top of the existing
 > [Android Engage SDK](https://developer.android.com/guide/playcore/engage) to
-> improve usability in other Android app-building frameworks and provide an
-> idiomatic Kotlin API over the tedious Java APIs.
+> provide an idiomatic Kotlin API over the tedious Java APIs and also improve
+> usability in other Android app-building frameworks.
--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,28 +1,60 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 import starlight from "@astrojs/starlight";
+
+import starlightVersions from "starlight-versions";
+import starlightCoolerCredit from "starlight-cooler-credit";
+
+const githubBaseUrl = "https://github.com/vighnesh153/google-video-discovery";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://googlevideodiscovery.vighnesh153.dev",
   integrations: [
     starlight({
       title: "Google Video Discovery",
-      social: {
-        github: "https://github.com/vighnesh153/google-video-discovery",
+      logo: {
+        light: "./src/assets/logo-light.svg",
+        dark: "./src/assets/logo-dark.svg",
+        replacesTitle: true,
       },
+      social: {
+        github: githubBaseUrl,
+        linkedin: "https://www.linkedin.com/in/vighnesh153",
+        email: "mailto:pikachu@vighnesh153.dev",
+      },
+      editLink: {
+        baseUrl: `${githubBaseUrl}/edit/main/docs`,
+      },
+      customCss: [
+        "./src/tailwind.css",
+      ],
+      plugins: [
+        starlightCoolerCredit(),
+        // starlightVersions({
+        //   versions: [{
+        //     slug: "v0.x",
+        //   }],
+        // }),
+      ],
       sidebar: [
         {
           label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Example Guide", slug: "guides/example" },
-          ],
+          autogenerate: { directory: "guides" },
+          // items: [
+          //   // Each item here is one entry in the navigation menu.
+          //   { label: "Example Guide", slug: "guides/example" },
+          // ],
         },
         {
           label: "Reference",
           autogenerate: { directory: "reference" },
         },
       ],
+    }),
+    tailwind({
+      applyBaseStyles: false,
     }),
   ],
 });
